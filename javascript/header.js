089ce16d3815942c920e4583bdf32a12f4dd809e
Ziliao Jian <ziliaoj@student.unimelb.edu.au>
2022-05-30 02:09:39 +0800

    finish the homepage and add menu interaction using js

--- javascript/header.js
+++ javascript/header.js
@@ -5,3 +5,7 @@ function goHome() {
 function goCart() {
     window.location.href ="../html/shopping-cart.html"
 }
+
+function showNav() {
+    document.getElementById("dropdown").classList.toggle("show");
+  }
